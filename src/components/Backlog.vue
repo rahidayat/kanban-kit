74a1a5814ac7d493c9c418efa31611aa40efd291
rahid <hidayat.85@gmail.com>
2017-10-08 16:13:23 +0700

    fetch backlog tasks from firebase and show them

--- src/components/Backlog.vue
+++ src/components/Backlog.vue
@@ -4,13 +4,27 @@
     <h3 class="panel-title">Back-log</h3>
   </div>
   <div class="panel-body">
-    Panel content
+    <TaskList :tasks="backlog" :status="status"/>
   </div>
 </div>
 </template>
 
 <script>
+import TaskList from '@/components/TaskList'
 export default {
+  components: {
+    TaskList
+  },
+  data () {
+    return {
+      status: 'backlog'
+    }
+  },
+  firebase () {
+    return {
+      backlog: this.$fbase.ref('task/backlog/')
+    }
+  }
 }
 </script>
 
---
+++ src/components/TaskList.vue
@@ -0,0 +1,40 @@
+<template lang="html">
+  <div class="">
+    <div class="panel panel-default" v-for="task in tasks">
+      <div class="panel-heading">{{task.title}}</div>
+      <div class="panel-body">
+        <p>Point : {{task.point}}</p>
+        <p>Assigned to : {{task.assignedTo}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['tasks', 'status'],
+  components: {
+    //
+  },
+  data () {
+    return {
+      detail: {}
+    }
+  },
+  methods: {
+    getDetail (id) {
+      for (let i = 0; i < this.tasks.length; i++) {
+        if (id === this.tasks[i]['.key']) {
+          this.detail = this.tasks[i]
+        }
+      }
+    }
+  }
+  // created () {
+  //   this.getDetail()
+  // }
+}
+</script>
+
+<style lang="css">
+</style>
