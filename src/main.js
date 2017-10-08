cbc9b3a094b44d752d0e93be16a510b92c604e65
rahid <hidayat.85@gmail.com>
2017-10-08 13:27:09 +0700

    initial setup with firebase and vuefire

---
+++ src/main.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import VueFire from 'vuefire'
+import firebase from 'firebase'
+
+Vue.use(VueFire)
+Vue.config.productionTip = false
+
+var config = {
+  apiKey: "AIzaSyAAmZPZXnAIXJYHAMa28MGsSnKxJqL0EkU",
+  authDomain: "kanban-kit.firebaseapp.com",
+  databaseURL: "https://kanban-kit.firebaseio.com",
+  projectId: "kanban-kit",
+  storageBucket: "kanban-kit.appspot.com",
+  messagingSenderId: "570294804483"
+};
+
+firebase.initializeApp(config);
+Vue.prototype.$db = firebase.database()
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
